73830bb01e481ce6ce2df5afed4eeda56a1c4b5e
Nat <nat@cmmc.io>
2018-04-15 20:30:34 +0700

    refactor: prepare data.

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ let params = {
   stationId: '35t',
   action: 'showTable',
   reportType: 'Raw',
-  endDate: '2017-04-30',
-  startDate: '2016-04-15',
+  endDate: '2018-04-30',
+  startDate: '2018-04-15',
   startTime: '00:00:00',
   endTime: '00:00:00',
   dataReportType: '_h',
@@ -32,64 +32,66 @@ let form = new FormData()
 Object.entries(params).forEach(([key, value]) => form.append(key, value))
 let headers = form.getHeaders()
 
+let sensorTitle
 fetch('http://aqmthai.com/includes/getMultiManReport.php', {
   method: 'POST', body: form, headers
 }).then(res => res.text())
   .then(body => {
     const obj = parse(body)
-    const trTags = obj.root.children[2].children
-    const trHeader = trTags.shift().children
-    const sensorTitle = trHeader.map(val => {
+    const rows = obj.root.children[2].children
+    const trHeader = rows.shift().children
+    sensorTitle = trHeader.map(val => {
       const [field1, field2] = val.content.split('_')
-      return (field2 || field1)
+      return field2 || 'time'
     })
-
-    // remove average fields
-    trTags.pop()
-    trTags.pop()
-    trTags.pop()
-    trTags.pop()
-    trTags.pop()
-    let total = trTags.length
-    console.log(`found ${total} rows`)
-
-    let rows = trTags.map((v) => {
+    rows.splice(-5) // remove average fields 5 last fields
+    return rows
+  })
+  .then(rows => {
+    console.log(`tr tags = ${rows.length}`)
+    return rows.map((v) => {
       let out = {}
-      v.children.forEach((v, idx) => {
-        let content = v.content
-        if (idx === 0) {
-          const fck = content.split(',').map(v => parseInt(v, 10))
-          fck [1] -= 1
-          content = moment.tz(fck, 'Asia/Bangkok')
-        }
-        else {
-          content = parseFloat(content) || 0
-        }
-        out[sensorTitle[idx]] = content
-      })
+      let c = v.children.shift().content
+      let [y, m, d, hh, mm, ss] = c.split(',')
+      let dField = moment.tz([y, m - 1, d, hh, mm, ss], 'Asia/Bangkok').toDate()
+      let values = v.children.map(v => parseFloat(v.content) || undefined)
+      let z = [dField, ...values]
+      z.forEach((sensorValue, idx) => out[sensorTitle[idx]] = sensorValue)
       return out
     })
-
-    rows.forEach((row, idx) => {
-      let data = Object.assign({}, row)
-      let ts = data['Date Time'].toDate()
-      let d = data['Date Time']
-      delete data['Date Time']
-      setTimeout(() => {
-        console.log(`writing.... [${d}] -- ${((idx + 1) * 100 / total).toFixed(2)}% [${idx}/${total}] `)
-        influx.writePoints([
-          {
-            measurement: 'aqm',
-            tags: {
-              stationId: params.stationId
-            },
-            fields: data,
-            timestamp: ts,
-          }
-        ], {
-          precision: 's',
-          database: 'aqithaidb',
-        })
-      }, idx * 50)
-    })
   })
+  .then(rows => {
+    console.log(rows)
+    // return rows.map((v, idx) => {
+    //   let out = {}
+    //   out[sensorTitle[idx]] = v
+    //   return out
+    // })
+  })
+  .then(rows => {
+    // console.log(rows)
+  })
+
+// // writeDb
+// rows.forEach((row, idx) => {
+//   let data = Object.assign({}, row)
+//   let ts = data['Date Time'].toDate()
+//   let d = data['Date Time']
+//   delete data['Date Time']
+//   setTimeout(() => {
+//     console.log(`writing.... [${d}] -- ${((idx + 1) * 100 / total).toFixed(2)}% [${idx}/${total}] `)
+//     influx.writePoints([
+//       {
+//         measurement: 'aqm',
+//         tags: {
+//           stationId: params.stationId
+//         },
+//         fields: data,
+//         timestamp: ts,
+//       }
+//     ], {
+//       precision: 's',
+//       database: 'aqithaidb',
+//     })
+//   }, idx * 50)
+// })
