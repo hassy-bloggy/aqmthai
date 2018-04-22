7f3b073ff66607da49428431c45c997ee0610fb6
Nat <nat@cmmc.io>
2018-04-22 15:50:56 +0700

    update

--- index.js
+++ index.js
@@ -3,14 +3,49 @@ let FormData = require('form-data')
 let parse = require('xml-parser')
 let inspect = require('util').inspect
 let moment = require('moment-timezone')
-
+const influxHost = process.env.INFLUX_HOST
+const influxUsername = process.env.INFLUX_USERNAME
+const influxPassword = process.env.INFLUX_PASSWORD
+const influxDbName = process.env.INFUX_DBNAME
 const Influx = require('influx')
 const influx = new Influx.InfluxDB({
-  hosts: [{host: 'localhost', port: 8086}],
-  username: 'nat',
-  password: 'nattan',
-  database: 'aqmthai_comdb'
+  hosts: [{host: influxHost, port: 8086}],
+  username: influxUsername,
+  password: influxPassword,
+  database: influxDbName
 })
+
+console.log(process.env)
+
+const insertDb = rows => {
+  const total = rows.length
+  const delayMs = 50
+  rows.forEach((row, idx) => {
+    const stationId = row.stationId
+    const ts = row.time
+    delete row.time
+    delete row.stationId
+    setTimeout(() => {
+      console.log(`writing.... [${stations[stationId]}] [${ts}] -- ${((idx + 1) * 100 / total).toFixed(2)}% [${idx + 1}/${total}] `)
+      influx.writePoints([
+        {
+          measurement: 'aqm',
+          tags: {
+            stationId: stations[stationId]
+          },
+          fields: row,
+          timestamp: ts,
+        }
+      ], {
+        precision: 's',
+        database: 'aqmthai_comdb',
+      })
+    }, idx * delayMs)
+  })
+}
+
+const endDate = '2017-12-31'
+const startDate = '2017-01-01'
 
 let stations = {
   '03t': '03t ริมถนนกาญจนาภิเษก เขตบางขุนเทียน กรุงเทพ',
@@ -70,83 +105,72 @@ let stations = {
   // 'm8': 'm8 หน่วยตรวจวัดเคลื่อนที่ 8 ค่ายมหาสุรสิงหนาท จ.ระยอง (เริ่ม 19 เมษายน 60)',
 }
 
-Object.entries(stations).forEach(([stationId, value], majorIdx) => {
-  console.log(`${majorIdx} -> ${value}`)
-  setTimeout(() => {
-    console.log(`starting job ${majorIdx + 1}/${Object.entries(stations).length }`)
-    let params = {
-      paramValue: 'CO,NO,NOX,NO2,SO2,O3,PM10,WD,TEMP,RH,SRAD,NRAD,BP,RAIN,WS,THC,PM2.5',
-      stationId: `${stationId}`,
-      action: 'showTable',
-      reportType: 'Raw',
-      endDate: '2017-12-31',
-      startDate: '2017-06-01',
-      startTime: '00:00:00',
-      endTime: '00:00:00',
-      dataReportType: '_h',
-      showNumRow: '100000',
-      pageNo: '1',
-    }
+let params = {
+  paramValue: 'CO,NO,NOX,NO2,SO2,O3,PM10,WD,TEMP,RH,SRAD,NRAD,BP,RAIN,WS,THC,PM2.5',
+  action: 'showTable',
+  reportType: 'Raw',
+  startTime: '00:00:00',
+  endTime: '00:00:00',
+  dataReportType: '_h',
+  showNumRow: '100000',
+  pageNo: '1',
+}
 
-    /* generate form data */
-    let form = new FormData()
-    Object.entries(params).forEach(([key, value]) => form.append(key, value))
-    let headers = form.getHeaders()
+const get = (params) => {
+  let body = new FormData()
+  let sensorTitleMap
+  let stationId = params.stationId
+  let stationName = params.stationName
+  Object.entries(params).forEach(([key, value]) => body.append(key, value))
+  return fetch('http://aqmthai.com/includes/getMultiManReport.php', {
+    method: 'POST', body, header: body.getHeaders()
+  }).then(res => res.text())
+    .then(body => {
+      const xml = parse(body)
+      const rows = xml.root.children[2].children
+      const trHeader = rows.shift().children
+      sensorTitleMap = trHeader.map(val => {
+        const [field1, field2] = val.content.split('_')
+        return field2 || 'time'
+      })
+      rows.splice(-5) // remove average fields 5 last fields
+      return rows
+    })
+    .then(rows => {
+      // console.log(`${params.stationName} found ${rows.length} data points.`)
+      console.log(`..... ${stationId} has been downloaded, len=${rows.length}.`)
+      return rows.map((v) => {
+        let row = {}
+        let c = v.children.shift().content
+        let [yyyy, m, d, hh, mm, ss] = c.split(',')
+        let dField = moment.tz([yyyy, m - 1, d, hh, mm, ss], 'Asia/Bangkok').toDate()
+        let values = v.children.map(v => parseFloat(v.content) || -1)
+        let z = [dField, ...values]
+        z.forEach((sensorValue, idx) => row[sensorTitleMap[idx]] = sensorValue)
+        row.stationId = stationId
+        return row
+      })
+    })
+  // .then(insertDb)
+}
 
-    let sensorTitle
-    fetch('http://aqmthai.com/includes/getMultiManReport.php', {
-      method: 'POST', body: form, headers
-    }).then(res => res.text())
-      .then(body => {
-        const obj = parse(body)
-        const rows = obj.root.children[2].children
-        const trHeader = rows.shift().children
-        sensorTitle = trHeader.map(val => {
-          const [field1, field2] = val.content.split('_')
-          return field2 || 'time'
-        })
-        rows.splice(-5) // remove average fields 5 last fields
-        return rows
-      })
-      .then(rows => {
-        console.log(`tr tags = ${rows.length}`)
-        return rows.map((v) => {
-          let out = {}
-          let c = v.children.shift().content
-          let [y, m, d, hh, mm, ss] = c.split(',')
-          let dField = moment.tz([y, m - 1, d, hh, mm, ss], 'Asia/Bangkok').toDate()
-          let values = v.children.map(v => parseFloat(v.content) || -1)
-          let z = [dField, ...values]
-          z.forEach((sensorValue, idx) => out[sensorTitle[idx]] = sensorValue)
-          return out
-        })
-      })
-      .then(rows => {
-        const total = rows.length
-        const delayMs = 50
-        rows.forEach((row, idx) => {
-          const ts = row.time
-          delete row.time
-          setTimeout(() => {
-            console.log(`writing.... [${stations[stationId]}] [${ts}] -- ${((idx + 1) * 100 / total).toFixed(2)}% [${idx + 1}/${total}] `)
-            influx.writePoints([
-              {
-                measurement: 'aqm',
-                tags: {
-                  stationId: stations[params.stationId]
-                },
-                fields: row,
-                timestamp: ts,
-              }
-            ], {
-              precision: 's',
-              database: 'aqithaidb',
-            })
-          }, idx * delayMs)
-        })
-      })
-      .then(rows => {
-        // console.log(rows)
-      })
-  }, majorIdx * 5300 * 55 + (15 * 1000))
-}) 
+const promises = Object.entries(stations).map(([stationId, stationName], majorIdx) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`starting job ${majorIdx + 1}/${Object.entries(stations).length } (${stationId})`)
+      Object.assign(params, {stationId, endDate, startDate, stationName})
+      get(params).then(resolve).catch(reject)
+    }, majorIdx * 5000)
+  })
+})
+
+Promise.all(promises).then(results => {
+  console.log(`all done. size = ${results.length}.`)
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  const lenArray = results.map(item => item.length)
+  const totalLen = lenArray.reduce(reducer)
+  console.log(`totalLen = ${totalLen}`)
+}).catch((err) => {
+  console.log('got error', err)
+})
+
